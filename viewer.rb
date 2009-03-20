acfe5f8e8c670d84eda7c61d0504ccc9bcc6c51a
Yutaka HARA <yutaka.hara+github@gmail.com>
2009-03-20 11:16:48 +0900

    戦略的デザイン修正

--- viewer.rb
+++ viewer.rb
@@ -14,10 +14,24 @@ end
 template :layout do
   <<-EOD
   <html>
-  <head><title>超戦略的ブロガー</title></head>
+  <head>
+    <title>超戦略的ブロガー</title>
+    <style>
+    body{ width:50%; margin:auto; }
+    #result{ border: 1px solid black; padding: 1em; margin: 1em; }
+    #description{ font-size: small; margin: 1em auto;}
+    #footer{ text-align: right; font-size: small; }
+    </style>
+  </head>
   <body>
-  <h1>超戦略的ブロガー</h1>
+  <h1><a href='.'>超戦略的ブロガー</a></h1>
   <%= yield %>
+  <div id='description'>
+    参考：<a href='http://kirik.tea-nifty.com/diary/2009/03/post-abbe.html'>切込隊長BLOG（ブログ） Lead‐off man's Blog: 戦略的馬鹿</a>
+  </div>
+  <div id='footer'>
+    powered by <a href='http://route477.net/'>Route477.net</a>
+  </div>
   </body>
   </html>
   EOD
@@ -26,7 +40,9 @@ end
 get '*' do
   erb <<-EOD
   <form method='POST'>
-  <textarea name='text' cols='80' rows='20'></textarea>
+  <textarea name='text' cols='80' rows='20'>
+このサービスは任意の文章を知的かつストラテジックに変換します。
+  </textarea>
   <input type='submit' value='変換'>
   </form>
   EOD
@@ -35,10 +51,13 @@ end
 post '*' do
   @text = Senryakuteki.convert(params['text'])
   erb <<-EOD
-  <div>
+  <div id='result'>
     <%= h @text %>
   </div>
 
-  <a href='.'>別の文章を変換する</a>
+  <div style='margin-left: 1em;'>
+    <a href='.'>別の文章を変換する</a>
+  </div>
+
   EOD
 end
